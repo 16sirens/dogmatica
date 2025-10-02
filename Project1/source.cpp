851817b326fc260d515678169b54ddd0026fce3d
16sirens <233814410+16sirens@users.noreply.github.com>
2025-10-02 20:58:21 +0100

    person 2 wrote stuff

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -6,5 +6,6 @@ int main() {
 	cout << "ok but what why???" << endl;
 	cout << "bigger headache than needed" << endl;
 	cout << "written by person 1" << endl;
+	cout << "written by person 2 wow!!" << endl;
 	return 0;
 }
