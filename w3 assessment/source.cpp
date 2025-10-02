3589870c03693820cd60b1a64949cd96f34cab6d
16sirens <233814410+16sirens@users.noreply.github.com>
2025-10-02 22:40:27 +0100

    yeah whatever

--- w3 assessment/source.cpp
+++ w3 assessment/source.cpp
@@ -2,6 +2,25 @@
 using namespace std;
 
 int main() {
-	cout << "Hello World!";
+	// initialise n and ask for height of pyramid
+	int n = 0;
+	cout << "enter height: ";
+	cin >> n;
+	cout << endl;
+	// for loop to uh... tbc
+	for (; n >= 1; n--) {
+		for (int x = n; x >= 2; x--) {
+			cout << " ";
+		}
+		int i = 1;
+		while (i <= n) {
+			cout << i;
+			if (i <= n) {
+				i++;
+			}
+		}
+		cout << endl;
+	}
+	cout << "hep";
 	return 0;
 }
