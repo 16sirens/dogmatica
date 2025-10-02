b7c39ae3ecc1ffc8e9451161c789805a8043b3c8
16sirens <233814410+16sirens@users.noreply.github.com>
2025-10-02 20:54:00 +0100

    wrote some code

--- Project1/source.cpp
+++ Project1/source.cpp
@@ -5,5 +5,6 @@ int main() {
 	cout << "Hello World!" << endl;
 	cout << "ok but what why???" << endl;
 	cout << "bigger headache than needed" << endl;
+	cout << "written by person 1" << endl;
 	return 0;
 }
