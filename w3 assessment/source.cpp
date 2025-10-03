8a44e1a1d0975d1d3b8795d015b8d16ec1e0834a
16sirens <233814410+16sirens@users.noreply.github.com>
2025-10-03 19:41:11 +0100

    positions filled with spaces and 2n-1 chars

--- w3 assessment/source.cpp
+++ w3 assessment/source.cpp
@@ -3,47 +3,50 @@ using namespace std;
 
 int main() {
 	// initialise n and ask for height of pyramid
-	int height = 0;
+	int n = 0;
 	bool goodInput = false;
 
-	cout << "enter height: ";
-	cin >> height;
+	cout << "Enter the height of the pyramid (1-9): ";
+	cin >> n;
 	cout << endl;
 
 	// input validation - doesnt like characters or big numbers
 	while (goodInput == false) {
 
-		if (height >= 1 && height <= 9) {
+		if (n >= 1 && n <= 9) {
 			goodInput = true;
 		}
 		else {
 			cout << "Please enter a valid height (1-9): ";
-			cin >> height;
+			cin >> n;
 			cout << endl;
 		}
 	}
 
 	// for loop to cycle through each rows
-	for (int iRow = 1; iRow <= height; iRow++) {
+	for (int i = 1; i <= n; i++) {
 
-		// for loop for the spaces in each row
-		for (int space = 1; space <= height - iRow; space++) {
+		// for loop for the spaces in the start of each row
+		for (int space = 1; space <= n - i; space++) {
 			cout << " ";
 		}
 
 		int num = 1;
 		//one gazillion breakpoints later! it works!
-		bool mid = false;
-		for (int whatever = 1; whatever <= iRow * 2 - 1; whatever++) {
+		bool middle = false;
+		for (int whatever = 1; whatever <= i * 2 - 1; whatever++) {
 			
 			cout << num;
-			if (num < iRow && !mid) {
+			if (num < i && !middle) {
 				num++;
 			}
-			else if (num >= iRow || mid) {
-				mid = true;
+			else if (num >= i || middle) {
+				middle = true;
 				num--;
 			}
+		}
+		for (int space = 1; space <= n - i; space++) {
+			cout << " ";
 		}
 		cout << endl;
 	}
