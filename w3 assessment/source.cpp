042e7de2c3f1f2670a4c1fa14609a52001497f68
16sirens <233814410+16sirens@users.noreply.github.com>
2025-10-03 15:18:20 +0100

    added input validation kinda
    doesnt like characters or really big numbers

--- w3 assessment/source.cpp
+++ w3 assessment/source.cpp
@@ -4,12 +4,26 @@ using namespace std;
 int main() {
 	// initialise n and ask for height of pyramid
 	int n = 0;
+	bool goodInput = false;
+
 	cout << "enter height: ";
 	cin >> n;
 	cout << endl;
 
+	// input validation
+	while (goodInput == false) {
 
-	for (;n >= 0;n--){
+		if (n >= 1 && n <= 9) {
+			goodInput = true;
+		}
+		else {
+			cout << "Please enter a valid height (1-9): ";
+			cin >> n;
+			cout << endl;
+		}
+	}
+
+	for (; n >= 0; n--) {
 		int counter = 1;
 		int number = 1;
 		for (int i = 1; i <= n; i++) {
